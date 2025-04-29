9ef7a68d39b56cfe3ed39f44db2880d0415cd5e3
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-04-29 15:25:17 +0700

    create function print data

--- IntroductionOOP.cpp
+++ IntroductionOOP.cpp
@@ -7,6 +7,13 @@ public:
     int nim;
     string nama;
     float nilai;
+
+    void printData()
+    {
+        cout << "NIM :" << nim << endl;
+        cout << "Nama :" << nama << endl;
+        cout << "Nilai :" << nilai << endl;
+    }
 };
 
 int main()
